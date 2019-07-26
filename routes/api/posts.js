542fe64e3c858909b280a0ec01d09c02d1e32644
eneserdek <enesf.erdek@gmail.com>
2019-07-26 16:14:13 +0200

    Posts are likeable and unlikeable. One user can only make only like to a post and unlike his/her likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,114 @@ router.post(
   },
 );
 
+// @route     GET api/posts
+// @desc      GET all posts
+// @access    Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server Error`);
+  }
+});
+
+// @route     GET api/posts/:id
+// @desc      GET post by id
+// @access    Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).send(`No such post`);
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    // We are adding the following line to give error if id is not entered properly
+    if (err.kind === 'ObjectId') return res.status(404).send(`No such post`);
+
+    res.status(500).send(`Server Error`);
+  }
+});
+
+// @route     DELETE api/posts/:id
+// @desc      DELETE a post
+// @access    Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: `No such post` });
+
+    // Check on the user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: `User not authorized` });
+    }
+
+    await post.remove();
+
+    res.json(`Post removed`);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') return res.status(404).json({ msg: `No such post` });
+    res.status(500).send(`Server Error`);
+  }
+});
+
+// @route     PUT api/posts/like/:id
+// @desc      Like a post
+// @access    Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the user already liked the post
+    const check = post.likes.filter(like => like.user.toString() === req.user.id);
+
+    if (check.length > 0) return res.status(400).json({ msg: `Post already liked` });
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server Error`);
+  }
+});
+
+// @route     PUT api/posts/unlike/:id
+// @desc      Unlike a post
+// @access    Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the user already liked the post
+    const check = post.likes.filter(like => like.user.toString() === req.user.id);
+
+    if (check.length === 0) return res.status(400).json({ msg: `Post has not yet been liked` });
+
+    // Get remove index
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server Error`);
+  }
+});
+
 module.exports = router;
