67386dc67b80e22816cc289c2b72dca4c63bfe4d
eneserdek <enesf.erdek@gmail.com>
2019-08-02 19:05:02 +0200

    Now all profiles can be seen by ui

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 
 import PrivateRoute from './components/dashboard/routing/PrivateRoute';
 
@@ -44,6 +45,8 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+
               {/* Private routing is added. When a user logs out it automatically redirects to login page from dashboard. So to reach dashboard we need to be either authenticated or loading */}
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
