3380c5699f04c1b2dc2fff2d00aa8e5a2db4728f
eneserdek <enesf.erdek@gmail.com>
2019-08-02 17:41:24 +0200

    Users profile/All Users Profile/A selected users profile are added to the store

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 import { setAlert } from './alert';
+
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
+  GET_REPOS,
 } from './types';
 
-// GET CURRENT USERS PROFILE
+// Get current users profile
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('/api/profile/me');
+
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
@@ -24,8 +28,60 @@ export const getCurrentProfile = () => async dispatch => {
   }
 };
 
-// CREATE OR UPDATE A PROFILE
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
 
+  try {
+    const res = await axios.get('/api/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Create or update profile
 export const createProfile = (formData, history, edit = false) => async dispatch => {
   try {
     const config = {
@@ -40,6 +96,7 @@ export const createProfile = (formData, history, edit = false) => async dispatch
       type: GET_PROFILE,
       payload: res.data,
     });
+
     dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
     if (!edit) {
